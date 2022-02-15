2c914c51c5313320e2222b7dd154a1cbf3ac997e
angelo aguilar-morillo <angelo.aguilar-morillo@student.howest.be>
2022-02-15 15:57:44 +0100

    added basic functionality

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 from enum import Enum
 from VisualController import VisualController
+from GeneticAlgorithm import GeneticAlgorithm
+import pygame
 
 class GameState(Enum):
     INIT = 0
@@ -8,5 +10,28 @@ class GameState(Enum):
 class Program:
 
     def __init__(self):
-        self.VC = VisualController
+        self.VC = VisualController()
         self.currentStage = GameState.INIT
+        self.GA = GeneticAlgorithm()
+
+    def run(self):
+        self.VC.init()
+        nodes = []
+        while True:
+            self.VC.drawBackground()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    node = self.VC.getMousePos()
+                    nodes.append(node)
+
+            self.VC.drawNodes(nodes)
+            self.VC.updateScreen()
+
+
+
+program = Program()
+
+program.run()
