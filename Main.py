c2fb19b87a9a4f597f8b662e16fccc6326802b27
angelo aguilar-morillo <angelo.aguilar-morillo@student.howest.be>
2022-02-15 14:59:34 +0100

    added basic constructor for program

--- Main.py
+++ Main.py
@@ -1,5 +1,12 @@
 from enum import Enum
+from VisualController import VisualController
 
 class GameState(Enum):
     INIT = 0
     RUNNING = 1
+
+class Program:
+
+    def __init__(self):
+        self.VC = VisualController
+        self.currentStage = GameState.INIT
